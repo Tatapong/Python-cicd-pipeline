b863a6255382ee4a8e49db09d789ff4cacf018a4
Tatapong <info.salesvaccine@gmail.com>
2024-10-04 14:20:31 +0100

    updated app.py

--- gift-academy-app/app.py
+++ gift-academy-app/app.py
@@ -2,11 +2,13 @@ from http.server import SimpleHTTPRequestHandler
 from socketserver import TCPServer
 
 # Define the port number
-PORT = 8080
+PORT = 8081
 
 # Create the HTTP server
 Handler = SimpleHTTPRequestHandler
 
+# Bind the server to all network interfaces by using ""
+# This makes the server accessible from outside the container.
 with TCPServer(("", PORT), Handler) as httpd:
     print(f"Serving at port {PORT}")
     httpd.serve_forever()
